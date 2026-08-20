1bd790ef8eba06dcbff1c30f55f7c79418659d6f
kimdae993 <kimdae@hanyang.ac.kr>
2026-08-20 16:25:52 +0900

    [del] comment in server/config/dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -19,31 +19,13 @@ module.exports = {
     // 브로커 계정 정보는 고유해야함 -> 테스트 장비 매장
     mqttID: 'pnt',
     mqttPW: 'chai',
-
-    // 사원증(RFID) 매장 브로커ID
-
-    // 냉장 매장 고유번호
-    // divisionIdx: 'DI17647205538493077',
-    // 테스트용
-    // divisionIdx: 'DI17790813642907755', // 냉장 학습용 (여의도점)
-    // divisionIdx: 'DI17798460900133031', // 냉장 통테용 (부산점)
-    // divisionIdx: 'DI17790813642907755', // 냉동 테스트용
-    // divisionIdx: 'DI17846216186054023',
+    
     divisionIdx: 'DI17866730866133136', // 한양대 ITBT
     
-    // 냉장 장비 고유번호
-    // deviceIdx: 'DE17560868094789999',
-    // deviceIdx: 'DE17683631997086480',
-    // 테스트용
-    // deviceIdx: 'DE17790815108130388', // 냉장 학습용
-    // deviceIdx: 'DE17798461293792881', // 냉장 통테용
-    // deviceIdx: 'DE17815818605453073', // 냉동 테스트용 
-    // deviceIdx: 'DE17854776152298715',
     deviceIdx: 'DE17866731359563130', // 한양대 ITBT 냉동
     
     // PNT RestAPI 연결
     restApi: 'https://apichaidev.atcrk.co.kr/api/v1',
-    // userId: 'chaitest',   // 냉장용
     userId: 'chaitest2',   // 냉동용
     userPassword: 'iljin123!',
     get jwtToken() {
@@ -53,12 +35,10 @@ module.exports = {
         return process.env.JWT_TOKEN_AT;
     },
     ioboardApi: 'http://localhost:8000',
-    // ioboardApi: 'http://192.168.0.20:8000',
     // 카메라 서버 API 설정
     cameraApi: "http://localhost:8003",
     // 카드단말기 서버 API 설정
     cardTerminalApi: "http://localhost:8001",
-    // cardTerminalApi: "http://192.168.0.20:8001",
     // 모델 서버 API 설정
     modelApi: "http://localhost:8002",
 
@@ -66,9 +46,6 @@ module.exports = {
     get modelVersion() {
         return process.env.MODEL_VERSION;
     },
-    // modelVersion: "v1.0.0",
-    // storageType: "C",
-    // hasLoadcell: "Y",
     aiServerApi: 'http://139.150.8.82:2140',
 
     // RebootMqtt: 모델 코드/모델 저장 경로 및 engine 빌드 env 파일 경로
